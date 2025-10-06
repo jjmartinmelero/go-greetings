2e88bd117aed54aefaf1a0af3a9c6a1ac61d69f6
Juan Jesús Martín Melero <40661353+jjmartinmelero@users.noreply.github.com>
2025-10-06 13:18:44 +0000

    added main module

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jjmartinmelero/go-greetings
+
+go 1.24.5
---
+++ greetings.go
@@ -0,0 +1,16 @@
+package greetings
+
+import (
+	"errors"
+	"fmt"
+)
+
+func Hello(name string) (string, error) {
+
+	if name == "" {
+		return "", errors.New("Name cannot be empty")
+	}
+
+	message := fmt.Sprintf("Hi, %v, Welcome ! ", name)
+	return message, nil
+}
